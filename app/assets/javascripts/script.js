006c08a4b749b276c7887aa0753ca9ca4931ab84
michaeljymsgutierrez <michael@vielsoft.com>
2017-04-30 16:37:40 +0800

    Added js script for custom slider

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -5,9 +5,9 @@ console.log("Script has been loaded");
 $(document).ready(function(){
 	var x = $(window);
 	var pos = x.scrollTop();
-	console.log(pos);
+	// console.log(pos);
 	x.scroll(function(){
-		console.log(x.scrollTop());
+		// console.log(x.scrollTop());
 		if(x.scrollTop() >= 117) {
 			$('.video-header').fadeOut();
 			$('header').css('background-color','black');
@@ -18,3 +18,24 @@ $(document).ready(function(){
 		}
 	});
 });
+
+var image_dir = [
+	'/assets/Leche-Flan.jpg',
+	'/assets/ube-halaya.jpg',
+	'/assets/Puto.jpg'
+];
+var y = 0;
+setInterval(function(){
+	if(y > image_dir.length){
+		y = 0;
+	}
+	else{
+		var slider = document.getElementById('image-slider');
+		if(image_dir[y] != undefined){
+			slider.setAttribute('src',image_dir[y]);
+			console.log(slider);
+		}
+		y++;
+	}
+	
+},3000);
